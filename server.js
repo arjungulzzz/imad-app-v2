297f682b3b8a3b653c7b89527e2e039f017f251d
Arjun Gulyani <arjungulzzz@gmail.com>
2017-02-05 20:25:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res) {
-    res.send('The article one will be served here'); //plaintext response
+    res.sendFile(path.join(__dirname, 'ui', article-one.html));
 });
 
 app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', article-one.html));
+    res.send('Article two will be served here');
 });
 
 app.get('/article-three',function(req,res){
