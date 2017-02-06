fee5621d50a7d70edf6fad5246e8ea24e80351bc
Arjun Gulyani <arjungulzzz@gmail.com>
2017-02-06 13:15:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,64 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var articleOne = {
+    
+    title:'Article-One | Arjun',
+    heading:'Article one',
+    date:'Feb 5 2017',
+    content:`
+                <p> This is some content This is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some content
+                </p>
+                
+                <p> This is some content This is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some content
+                </p>
+                
+                <p> This is some content This is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some content
+                </p>
+                
+                <p> This is some content This is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some contentThis is some content
+                </p>`
+};
+
+function createTemplate(data) {
+        var title = data.title;
+        var heading = data.heading;
+        var date = data.date;
+        var content = data.content;
+        var HTMLTemplate= `
+        <html>
+            <head>
+                <title>${title}</title>
+                <meta name="viewport" conten="width=device-width, initial-scale=1"/>
+                <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            <body>
+                <div class="container">
+                    <div>
+                        <a href="/">Home</a>
+                    </div>
+                    <hr/>
+                    <h3>
+                        ${heading}
+                    </h3>
+                    <div>
+                        ${date}
+                    </div>
+                    <div>
+                        ${content}
+                    </div>
+                </div>
+            </body>
+            
+        </html>
+`;
+    return HTMLTemplate;
+}
+
+
+
 app.get('/article-one',function(req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne)));
 });
 
 app.get('/article-two',function(req,res){
