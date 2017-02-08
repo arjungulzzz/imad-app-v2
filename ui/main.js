008df3399ea8af19cf276eb95794facc33206f66
Arjun Gulyani <arjungulzzz@gmail.com>
2017-02-08 16:17:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,16 @@ element.innerHTML = 'New Content';
 
 var image = document.getElementById('madi');
 
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft +10;
+    image.style.marginLeft = marginLeft + 'px';
+}
+
+
+
 image.onclick = function() {
-    
-  image.style.marginLeft = '100px';  
-};
+  var interval = setInterval(moveRight,100);
+  
+  };
+
