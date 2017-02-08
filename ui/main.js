afde350df1ca70d4aca3624128221f4a7d334a6a
Arjun Gulyani <arjungulzzz@gmail.com>
2017-02-08 22:21:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,25 @@ image.onclick = function() {
   
   };
 
+var button = document.getElementById('counter');
+button.onclick = function() {
+    
+    var request = new XMLHttpRequest(); // Create a request object
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //take some action
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //not done yet
+    };
+   
+    //make the request
+    request.open('GET', 'http://arjungulzzz.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
